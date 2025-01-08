5096d5d1381d5a269ce6089fc189f34018220e5b
Sadriddin1233 <166122700+Sadriddin1233@users.noreply.github.com>
2025-01-08 10:35:53 +0500

    app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ with open('houseprice.pkl', 'rb') as f:
 
 # Kiruvchi parametrlar
 st.title("Uy narxini bashorat qilish")
-bedrooms = st.number_input("Yotoqxonalar soni", min_value=1, step=1)
+bedrooms = st.number_input("Xonalar soni", min_value=1, step=1)
 bathrooms = st.number_input("Hammomlar soni", min_value=1, step=1)
 sqft_living = st.number_input("Yashash maydoni", min_value=500, step=50)
 floors = st.number_input("Qavatlar soni", min_value=1, step=1)
